6350859d9348ed126695e25f0656613b2765ad10
Vitali Aleksandruk <60776251+vVitalix@users.noreply.github.com>
2022-12-16 21:10:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# GraphQL server for Minimalistic Storefront
+
+### Tech stack
+
+```
+- TypeScript
+- Apollo Server
+```
+
+### How to start
+
+```
+In order to start the server, follow these steps:
+
+1. Install dependencies
+2. Build the project - `yarn build`
+3. Start the project - `yarn start`
+```
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from "apollo-server";
+
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+
+const server = new ApolloServer({
+    introspection: true,
+    playground: true,
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+});
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
